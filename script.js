04435e7e22121a5742e75766bfa785921b33573e
Quenton McDaniel <96561883+QMcD@users.noreply.github.com>
2023-12-20 20:49:57 -0600

    Added playGame function to play one round

--- script.js
+++ script.js
@@ -4,3 +4,20 @@ function getComputerChoice(){
     return choices[randomChoice];
 }
 console.log(getComputerChoice())
+
+function playGame(){
+    let playerChoice = 'paper';
+    let computerChoice = getComputerChoice()
+
+    if(playerChoice === computerChoice){
+        return 'Draw' 
+    }else if(playerChoice === 'rock' && computerChoice === 'scissors'){
+        return `You Win! ${playerChoice} beats ${computerChoice}` 
+    }else if(playerChoice === 'scissors' && computerChoice === 'paper'){
+        return `You Win! ${playerChoice} beats ${computerChoice}`
+    }else if(playerChoice === 'paper' && computerChoice === 'rock'){
+        return `You Win! ${playerChoice} beats ${computerChoice}`
+    }else
+        return `You Lose ${computerChoice} beats ${playerChoice}`
+    
+};
