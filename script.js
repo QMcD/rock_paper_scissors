f8ebd611e5f8ae55ccd524ff8e4e9078bf54ad04
Quenton McDaniel <96561883+QMcD@users.noreply.github.com>
2023-12-31 16:25:01 -0600

    Cosmetic formatting changes

--- script.js
+++ script.js
@@ -10,51 +10,51 @@ function playGame(playerChoice, computerChoice){
     if(playerChoice === computerChoice){
         return playGame(prompt("Draw! Choose again!").toLowerCase(), getComputerChoice()) 
     }else if(playerChoice === 'rock' && computerChoice === 'scissors'){
-        return `You Win! ${playerChoice} beats ${computerChoice}!` 
+        return `You Win! ${playerChoice} beats ${computerChoice}!`;
     }else if(playerChoice === 'scissors' && computerChoice === 'paper'){
-        return `You Win! ${playerChoice} beats ${computerChoice}!`
+        return `You Win! ${playerChoice} beats ${computerChoice}!`;
     }else if(playerChoice === 'paper' && computerChoice === 'rock'){
-        return `You Win! ${playerChoice} beats ${computerChoice}!`
+        return `You Win! ${playerChoice} beats ${computerChoice}!`;
     }else{
-        return `You Lose! ${computerChoice} beats ${playerChoice}!`
+        return `You Lose! ${computerChoice} beats ${playerChoice}!`;
     }    
-};
+}
 
 //Global Variables for bestFive functions
-let roundCounter = 0
-let computerScore = 0
-let playerScore = 0
+let roundCounter = 0;
+let computerScore = 0;
+let playerScore = 0;
 
 //Determine the winner and display message
 function showVictor(){
     if(playerScore > computerScore){
-        message = `You Win!\nYou: ${playerScore} | Computer: ${computerScore}`
-        return message
+        message = `You Win!\nYou: ${playerScore} | Computer: ${computerScore}`;
+        return message;
     }else{
-        message = `Computer Wins!\nComputer: ${computerScore} | You: ${playerScore}`
-        return message
+        message = `Computer Wins!\nComputer: ${computerScore} | You: ${playerScore}`;
+        return message;
     }
-}
+};
 
 //Play rock, paper, scissors five times
 function playFiveGames(){
 
-    roundCounter++
+    roundCounter++;
     const playerChoice = prompt("Choose your weapon!", "rock, paper, or scissors").toLowerCase();
-    const computerChoice = getComputerChoice()
-    const round = playGame(playerChoice, computerChoice)
+    const computerChoice = getComputerChoice();
+    const round = playGame(playerChoice, computerChoice);
     
     if(round === `You Lose! ${computerChoice} beats ${playerChoice}!`){
-        computerScore++
+        computerScore++;
     }else{
-        playerScore++
+        playerScore++;
     }
 
     if(roundCounter < 5){
-        return playFiveGames()
+        return playFiveGames();
     }
-    return showVictor()
+    return showVictor();
 }
 
-console.log(playFiveGames())
+console.log(playFiveGames());
 
